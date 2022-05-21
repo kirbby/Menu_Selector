f8f97e6a51ddcdb822dfd43c39219b1a5ce9c223
kirbby <develop@m-winkler.at>
2022-05-21 22:29:15 +0200

    Add MenuItem to HomePage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <MenuItem
+            :menu-item="menuItem"
+            v-for="menuItem in menuItems"
+            :key="menuItem.id"
+        >
+        </MenuItem>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import MenuItem from "@/components/MenuItem.vue";
+import MenuItemType from "@/types/MenuItem";
+
+export default defineComponent({
+    components: {
+        MenuItem,
+    },
+    setup() {
+        const menuItems = ref<MenuItemType[]>([]);
+
+        return {
+            menuItems,
+        };
+    },
+});
+</script>
+
+<style scoped lang="postcss"></style>

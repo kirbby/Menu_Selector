5583814f7be4d261b5a9ff3d736fbc4306b14517
Dominik Mlasko <dominik.mlasko@gmail.com>
2022-06-05 19:22:17 +0200

    Add Log outout

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,22 +13,31 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	PORT = 8080
+)
+
 func main() {
-    // Create ent client.
-    client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
-    if err != nil {
-        log.Fatal(err)
-    }
-    // Run the migrations.
-    if err := client.Schema.Create(context.Background(), schema.WithAtlas(true)); err != nil {
-        log.Fatal(err)
-    }
-    // Start listening.
-    srv, err := ogent.NewServer(ogent.NewOgentHandler(client))
-    if err != nil {
-        log.Fatal(err)
-    }
-    if err := http.ListenAndServe(":8001", srv); err != nil {
-        log.Fatal(err)
-    }
+	fmt.Print("Starting backend ...\n")
+
+	// Create ent client.
+	client, err := ent.Open(dialect.SQLite, "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Run the migrations.
+	if err := client.Schema.Create(context.Background(), schema.WithAtlas(true)); err != nil {
+		log.Fatal(err)
+	}
+	// Start listening.
+	srv, err := ogent.NewServer(ogent.NewOgentHandler(client))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Listening on Port %d\n", PORT)
+	if err := http.ListenAndServe(fmt.Sprintf(": %d", PORT), srv); err != nil {
+		log.Fatal(err)
+	}
+
 }

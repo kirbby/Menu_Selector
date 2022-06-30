149d4f4a2bfa2a1758b85e9263e3b02e9f93d01c
kirbby <develop@m-winkler.at>
2022-06-30 16:08:21 +0200

    Move supabase credentials to environment variable

--- frontend/src/supabaseClient.ts
+++ frontend/src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    "https://jqqgchderbtpopobtzar.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxcWdjaGRlcmJ0cG9wb2J0emFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU5OTMwMzUsImV4cCI6MTk3MTU2OTAzNX0.oaTnuAYA2nBsFUvvb4dhCVYsXTf3d_h5uAxkTECKPOE");
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY);
 
 export default supabase;
